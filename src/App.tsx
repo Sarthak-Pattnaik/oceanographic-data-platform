75291ad5d4e9dbf4a21706e6929bc173f0b86c7e
Sarthak Pattnaik <psarthak@protonmail.com>
2025-09-16 10:05:39 +0530

    removed extra files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import DataExplorer from "./pages/DataExplorer";
-import TaxonomyExplorer from "./pages/TaxonomyExplorer";
 
 
 function App() {
   return (
     <Routes>
+      _{/*
         <Route path="/" element={<Home />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/DataExplorer" element={<DataExplorer />} />
         <Route path="/TaxonomyExplorer" element={<TaxonomyExplorer />} />
+        */}
     </Routes>
   );
 }
